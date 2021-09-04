7f3f347063962b5c24fa75ef3d0dfa7d0cf0c475
Sean Carolan <sean.carolan@hashicorp.com>
2021-09-04 11:44:54 -0500

    Replace more hyphens. Down with the hyphen.

--- setup_templates/linux_vm/outputs.tf
+++ setup_templates/linux_vm/outputs.tf
@@ -1,8 +1,8 @@
 # Outputs file
 output "catapp_url" {
-  value = "http://${azurerm_public_ip.tflab-pip.fqdn}"
+  value = "http://${azurerm_public_ip.tflab_pip.fqdn}"
 }
 
 output "catapp_ip" {
-  value = "http://${azurerm_public_ip.tflab-pip.ip_address}"
+  value = "http://${azurerm_public_ip.tflab_pip.ip_address}"
 }
